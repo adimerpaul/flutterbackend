da61542d7beb3070dc0b934b65b8c4109210d04b
adimer <adimer101@gmail.com>
2024-10-21 21:30:46 -0400

    Actulizado para creacion de api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
-{
-    //
+class ProductController extends Controller{
+    function index(){
+        return Product::all();
+    }
+    function store(Request $request){
+        return Product::create($request->all());
+    }
+    function update(Request $request, Product $product){
+        $product->update($request->all());
+        return $product;
+    }
+    function destroy(Product $product){
+        $product->delete();
+        return $product;
+    }
 }
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+Route::get('products', [ProductController::class, 'index']);
+Route::post('products', [ProductController::class, 'store']);
+Route::put('products/{product}', [ProductController::class, 'update']);
+Route::delete('products/{product}', [ProductController::class, 'destroy']);
