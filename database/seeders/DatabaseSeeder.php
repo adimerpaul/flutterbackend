5ad107320467512018fd3add2b712d9856887e6e
adimer <adimer101@gmail.com>
2024-10-24 21:08:48 -0400

    creado seede de user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin')
         ]);
     }
 }
